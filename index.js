ccbff40588f504701f82f2c29c8a7da33483c8d6
duc2410 <tacongduc123@gmail.com>
2020-09-05 21:38:09 +0700

    modifi some files

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (rep, res) => res.send('hello world'));
-
+app.get('/', (rep, res) => res.send('hello wrld'));
 app.listen(port, () => console.log(`listen from port ${port}`))
