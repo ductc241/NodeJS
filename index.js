46e46aea9e0cd2ab08a6c1819c7ffba0354f98d0
duc2410 <tacongduc123@gmail.com>
2020-09-06 10:00:50 +0700

    add bootstrap and modifi some files

--- index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/', (rep, res) => res.send('hello world'));
-app.listen(port, () => console.log(`listen from port ${port}`))
