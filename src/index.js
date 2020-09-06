dd7c598c005ff12896e66e8e40e0e4a279bedbd8
duc2410 <tacongduc123@gmail.com>
2020-09-06 20:16:01 +0700

    adjust file tree and create 2 views

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
+const { Console } = require('console');
 const app = express();
 const port = 3000;
 
@@ -16,4 +17,13 @@ app.set('views', path.join(__dirname, 'resources\\views'));
 
 
 app.get('/', (rep, res) =>  res.render('home'));
+
+app.get('/search', (req,res) => {
+	res.render('search');
+});
+
+app.get('/result', (req,res) => {
+	console.log(req.query.q)
+})
+
 app.listen(port, () => console.log(`listen from port ${port}`))
